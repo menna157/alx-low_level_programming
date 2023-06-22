acfbce7c0d0d035abbbaa381ba8ef33ba714f3e8
menna157 <menna011tarek@gmial.com>
2023-06-22 10:44:35 -0700

    pp

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,20 +11,20 @@
 
 int main(void)
 {
- int i;
- unsigned long fib1 = 0, fib2 = 1, sum;
+int i;
+unsigned long fib1 = 0, fib2 = 1, sum;
 
- for (i = 1; i <= 50; i++)
- {
-  sum = fib1 + fib2;
-  printf("%lu", sum);
+for (i = 1; i <= 50; i++)
+{
+sum = fib1 + fib2;
+printf("%lu", sum);
 
-  fib1 = fib2;
-  fib2 = sum;
-  if (i == 50)
-   printf("\n");
-  else
-   printf(", ");
- }
- return (0);
+fib1 = fib2;
+fib2 = sum;
+if (i == 50)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
 }
