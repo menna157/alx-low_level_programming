b9af274a6668fd01ac2133bf9cc4a3945227621e
menna157 <menna011tarek@gmial.com>
2023-06-22 10:52:15 -0700

    pp

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,13 +13,13 @@
 
 int main(void)
 {
- int i, sum;
+int i, sum;
 
- for (i = 1; i <= 1024; i++)
- {
-  if (i % 3 == 0 || i % 5 == 0)
-   sum += i;
- }
- printf("%d\n", sum);
- return (0);
+for (i = 1; i <= 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+}
+printf("%d\n", sum);
+return (0);
 }
