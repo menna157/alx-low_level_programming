9faa028c87daf6719db8e7bdf013288771f48d68
menna157 <menna011tarek@gmial.com>
2023-06-22 17:04:43 -0700

    kk

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include  <stdio.h>
 
-  /**
+/**
  * main - causes an infinite loop
  * Return: 0
 */
@@ -12,7 +12,8 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0
+	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*     putchar(i);*/
