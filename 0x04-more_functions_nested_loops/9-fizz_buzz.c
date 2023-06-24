d3062459977ded97469b72bef56a1a2d956bf5d7
menna157 <menna011tarek@gmial.com>
2023-06-23 20:00:40 -0700

    pp

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,25 @@
 
 int main(void)
 {
-int i;
+ int i;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-printf("Buzz");
-else
-printf("%d", i);
-if (i != 100)
-printf(" ");
-else
-printf("\n");
-}
-return (0);
+ for (i = 1; i <= 100; i++)
+ {
+  if (i % 3 == 0 && i % 5 == 0)
+   printf("FizzBuzz");
+  else if (i % 3 == 0)
+   printf("Fizz");
+  else if (i % 5 == 0)
+   printf("Buzz");
+  else
+   printf("%d", i);
+  if (i != 100)
+   printf(" ");
+  else
+   printf("\n");
+ }
+ return (0);
 }
 Footer
+
+
