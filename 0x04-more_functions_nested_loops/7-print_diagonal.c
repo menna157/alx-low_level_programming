aca9b9ce39a65d555309826efc1265efe96d05ae
menna157 <menna011tarek@gmial.com>
2023-06-23 19:19:45 -0700

    pp

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_diagonal line
+ * print_diagonal - print a diagonal line
  *
  * @n: is the number of times the \ character
  *     should be printed
@@ -9,19 +9,18 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+int postn, space;
 
-	if (n <= 0)
-		_putchar('\n')
-	else
-	{
-
-			for (postn = 1; postn <= n; postn++)
-			{
-				for (space = 1; space <= postn; space++)
-					_putchar(' ');
-				_putchar(92); /*is equal to '/' char*/
-				_putchar('\n');
-	}
-	}
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (postn = 1; postn <= n; postn++)
+{
+for (space = 1; space <= postn; space++)
+_putchar(' ');
+_putchar(92); /*is equal to '/' char*/
+_putchar('\n');
+}
+}
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,18 +4,17 @@
  * print_square - print a square using the character #
  *
  * @size: is the size of the square
- *
- * Return: Always 0 (Success)
- */
+ * Return: Always 0 (success)
+*/
 
 void print_square(int size)
 {
-	int row, coulmn;
+int row, column;
 
-	for (row = 1; row <= size; row++)
-	{
-		for (coulmn = 1; coulmn <= size; coulmn++)
-			_putchar('#');
-		_putchar('\n');
-	}
+for (row = 1; row <= size; row++)
+{
+for (column = 1; column <= size; column++)
+_putchar('#');
+_putchar('\n');
+}
 }
