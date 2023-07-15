b8b3357ce0e013ca813544b2d9f2fe9b41b28c05
menna157 <menna011tarek@gmial.com>
2023-07-14 17:58:25 -0700

    pp

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,23 @@
 
 void print_triangle(int size)
 {
-int i, j;
+	int i, j;
 
-if (size <= 0)
-_putchar('\n');
-else
-{
-for (i = 1; i <= size; i++)
-{
-for (j = i; j <= size; j++)
-_putchar(' ');
-for (j = 1; j <= i; j++)
-_putchar('#');
-_putchar('\n');
-}
-}
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
