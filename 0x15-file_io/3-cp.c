3a556f222f876c8ce997215a0795c8d85cf4302a
menna157 <menna011tarek@gmail.com>
2023-08-07 18:55:45 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,7 @@
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 
-#define ERR NOCLOSE "Error: Can't close fd %d\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
