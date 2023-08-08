9603b2d8c4eb9eb98a61b6013b60ea549baa6778
menna157 <menna011tarek@gmail.com>
2023-08-07 17:31:13 -0700

    oo

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ if (!filename)
 
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 if (fd == -1)
 
